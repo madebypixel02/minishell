21756d7c472f17e1e58b82a34a435cafceca88bf
mbueno-g <buenogarciamarina@gmail.com>
2021-11-08 20:06:13 +0100

    Implemented ~ for ls + created expand.c in previous commit :)

--- src/expand.c
+++ src/expand.c
@@ -6,46 +6,35 @@
 /*   By: mbueno-g <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/08 18:17:55 by mbueno-g          #+#    #+#             */
-/*   Updated: 2021/11/08 19:35:19 by mbueno-g         ###   ########.fr       */
+/*   Updated: 2021/11/08 20:05:19 by mbueno-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
-
-static char	*get_substr_path(char *str, int len[3], int ij[2], int quotes[2])
-{
+char	*expand_path(char *str, int ij[2], int quotes[2])
+{	
 	char	*exp1;
+	char	*path;
 
+	quotes[0] = 0;
+	quotes[1] = 0;
 	ij[0] = -1;
 	exp1 = NULL;
 	while (++ij[0] >= 0 && str && str[ij[0]])
 	{
 		quotes[0] = (quotes[0] + (!quotes[1] && str[ij[0]] == '\'')) % 2;
 		quotes[1] = (quotes[1] + (!quotes[0] && str[ij[0]] == '\"')) % 2;
-		if ((!quotes[0] && !quotes[1]) && ((str[ij[0]] == '~' && str[ij[0] + 1] == '\0')))
+		if ((!quotes[0] && !quotes[1]) && ((str[ij[0]] == '~')))
 		{
-			exp1 = getenv("HOME");
-			//free(str);
+			path = getenv("HOME");
+			exp1 = ft_strjoin(path, str + 1);
+			free(str);
+			if (access(exp1, F_OK) == -1)
+				return (NULL);
 			break ;
 		}
 	}
-	return (exp1);
-}
-
-char	*expand_path(char *str, int ij[2], int quotes[2])
-{	
-	int		len[3];
-	char	*exp1;
-	char	*ptr;
-
-	quotes[0] = 0;
-	quotes[1] = 0;
-	ij[0] = -1;
-	ij[1] = -2;
-	len[0] = ft_strlen(str);
-	len[2] = 0;
-	exp1 = get_substr_path(str, len, ij, quotes);
 	if (!exp1)
 		return (str);
 	return (exp1);
