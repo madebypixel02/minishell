a283314a4e2a43b6385f4b94759135a993115177
madebypixel02 <madebypixel02@gmail.com>
2021-11-18 10:45:11 +0100

    Fixed readline colors causing issues in prompt

--- inc/colors.h
+++ inc/colors.h
@@ -6,7 +6,7 @@
 /*   By: aperez-b <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/22 17:30:20 by aperez-b          #+#    #+#             */
-/*   Updated: 2021/11/14 12:31:14 by aperez-b         ###   ########.fr       */
+/*   Updated: 2021/11/18 10:44:01 by aperez-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,14 @@
 # define COLORS_H
 
 /* Color codes for users  */
-# define DEFAULT "\033[0;39m"
-# define GRAY "\033[1;90m"
-# define RED "\033[1;91m"
-# define GREEN "\033[1;92m"
-# define YELLOW "\033[1;93m"
-# define BLUE "\033[1;94m"
-# define MAGENTA "\033[1;95m"
-# define CYAN "\033[1;96m"
-# define WHITE "\033[0;97m"
+# define DEFAULT "\001\033[0;39m\002"
+# define GRAY "\001\033[1;90m\002"
+# define RED "\001\033[1;91m\002"
+# define GREEN "\001\033[1;92m\002"
+# define YELLOW "\001\033[1;93m\002"
+# define BLUE "\001\033[1;94m\002"
+# define MAGENTA "\001\033[1;95m\002"
+# define CYAN "\001\033[1;96m\002"
+# define WHITE "\001\033[0;97m\002"
 
 #endif
