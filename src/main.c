c497cf9d2d703cc494d5803c845b4676a9366b8b
madebypixel02 <madebypixel02@gmail.com>
2021-11-22 20:13:42 +0100

    Fix default value for PWD

--- src/main.c
+++ src/main.c
@@ -6,20 +6,16 @@
 /*   By: aperez-b <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/22 13:40:47 by aperez-b          #+#    #+#             */
-/*   Updated: 2021/11/22 18:52:58 by aperez-b         ###   ########.fr       */
+/*   Updated: 2021/11/22 20:13:18 by aperez-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
-static t_prompt	init_prompt(char **argv, char **envp)
+static t_prompt	init_vars(t_prompt prompt, char *str, char **argv, char **envp)
 {
-	t_prompt	prompt;
-	char		*str;
-	char		*num;
+	char	*num;
 
-	prompt.cmds = NULL;
-	prompt.envp = ft_dup_matrix(envp);
 	str = getcwd(NULL, 0);
 	prompt.envp = mini_setenv("PWD", str, prompt.envp, 3);
 	free(str);
@@ -31,13 +27,28 @@ static t_prompt	init_prompt(char **argv, char **envp)
 	free(str);
 	prompt.envp = mini_setenv("SHLVL", num, prompt.envp, 5);
 	free(num);
-	prompt.envp = mini_setenv("PATH", \
+	str = mini_getenv("PATH", prompt.envp, 4);
+	if (!str)
+		prompt.envp = mini_setenv("PATH", \
 		"/usr/local/sbin:/usr/local/bin:/usr/bin:/bin", prompt.envp, 4);
+	free(str);
 	str = mini_getenv("_", prompt.envp, 1);
 	if (!str)
 		prompt.envp = mini_setenv("_", argv[0], prompt.envp, 1);
 	free(str);
+	return (prompt);
+}
+
+static t_prompt	init_prompt(char **argv, char **envp)
+{
+	t_prompt	prompt;
+	char		*str;
+
+	str = NULL;
+	prompt.cmds = NULL;
+	prompt.envp = ft_dup_matrix(envp);
 	prompt.e_status = 0;
+	prompt = init_vars(prompt, str, argv, envp);
 	return (prompt);
 }
 
